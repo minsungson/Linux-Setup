2b151c5e46b1ed85e5d2a8570954f64ea7a6055e
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-06-08 08:42:45 +0100

    fixed gruvbox link

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def extensions():
     webbrowser.open("https://extensions.gnome.org/extension-data/mediacontrolscliffniff.github.com.v20.shell-extension.zip")
 
 def themes():
-    webbrowser.open("https://ppload-com.s3.eu-central-1.amazonaws.com/data/files/1641887808/Gruvbox-Dark.zip?response-content-disposition=attachment%3B%2520Gruvbox-Dark.zip&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIATLQUPBWASZL2ZPWI%2F20220608%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20220608T072048Z&X-Amz-SignedHeaders=host&X-Amz-Expires=60&X-Amz-Signature=1cfb88d057aac38759a662d303c78dc05d90a1b404d3393291225fc335c77328") # gruvbox dark gtk theme
+    webbrowser.open("https://ppload-com.s3.eu-central-1.amazonaws.com/data/files/1641887808/Gruvbox-Dark.zip?response-content-disposition=attachment%3B%2520Gruvbox-Dark.zip&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIATLQUPBWASZL2ZPWI%2F20220608%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20220608T074209Z&X-Amz-SignedHeaders=host&X-Amz-Expires=60&X-Amz-Signature=e8543417b6b5efcbdffe9e69735ec11e800ecce9dbd19d9c91298204e2e6ca2a") # gruvbox dark gtk theme
 
     if not os.path.exists("/home/.themes"):
         os.makedirs("/home/.themes")
