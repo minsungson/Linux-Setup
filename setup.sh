f44fcd6b4c1e3a94aa5801e6c976bebffdf0cd1b
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-06-10 18:21:48 +0100

    snap removal, fingerprint driver install, auto reb

--- setup.sh
+++ setup.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 set -ex
 
+echo $"Machine will reboot after this script has been excecuted."
+
 # Ensure repositories are enabled
 sudo add-apt-repository universe
 sudo add-apt-repository multiverse
 sudo add-apt-repository restricted
 
-# Add dell drivers for Jammy XPS 13
+# Add fingerprint driver
 
-sudo sh -c 'cat oem-somerville-melisa-meta.list << EOF
-deb http://dell.archive.canonical.com/ oem-somerville-melisa-meta
-deb http://dell.archive.canonical.com/ jammy somerville
-# deb-src http://dell.archive.canonical.com/ focal somerville
-deb http://dell.archive.canonical.com/ jammy somerville-tentacool
-# deb-src http://dell.archive.canonical.com/ focal somerville-bulbasaur
-
-EOF'
-
+wget http://dell.archive.canonical.com/updates/pool/public/libf/libfprint-2-tod1-goodix/libfprint-2-tod1-goodix_0.0.4-0ubuntu1somerville1_amd64.deb
+sudo dpkg -i libfprint-2-tod1-goodix_0.0.4-0ubuntu1somerville1_amd64.deb
 
 sudo apt update && sudo apt upgrade
 
@@ -26,21 +21,30 @@ sudo snap remove emote
 sudo snap remove spotify
 sudo snap remove vlc
 sudo snap remove firefox
+sudo snap remove firefox
+sudo snap remove snap-store
+sudo snap remove gtk-common-themes
+sudo snap remove gnome-3-38-2004
+sudo snap remove core18
+sudo snap remove snapd-desktop-integration
 
-# Install fingerprint driver
-sudo apt install oem-somerville-melisa-meta libfprint-2-tod1-goodix oem-somerville-meta tlp-config -y
+# Remove Snap
 
-# Install fusuma for handling gestures
+sudo systemctl disable snapd.service
+sudo systemctl disable snapd.socket
+sudo systemctl disable snapd.seeded.service
 
-sudo gpasswd -a $USER input
-sudo apt install libinput-tools xdotool ruby -y -qq
-sudo gem install --silent fusuma
+sudo rm -rf /var/cache/snapd/
+sudo apt autoremove --purge snapd
+rm -rf ~/snap
 
-# Howdy for facial recognition
+sudo sh -c 'cat > /etc/apt/preferences.d/firefox-no-snap << EOF
 
-sudo add-apt-repository ppa:boltgolt/howdy -y
-sudo apt update -qq
-sudo apt install howdy -y
+Package: firefox*
+Pin: release o=Ubuntu*
+Pin-Priority: -1
+
+EOF'
 
 # Remove packages:
 
@@ -58,18 +62,15 @@ sudo apt install ubuntu-restricted-extras -y
 sudo apt install preload -y
 sudo apt install gnome-tweaks -y
 sudo apt install gnome-shell-extensions -y
-sudo apt install tlp tlp-rdw -y
-sudo systemctl enable tlp
-sudo systemctl start tlp
-sudo apt install bleachbit
-sudo apt-get install protonvpn
+sudo apt install bleachbit -y
+sudo apt install protonvpn
+sudo apt install libreoffice
 
 # Install apps with flatpak:
 
 flatpak install flathub org.mozilla.firefox
 flatpak install flathub com.visualstudio.code
 flatpak install flathub com.skype.Client
-flatpak install flathub org.onlyoffice.desktopeditors
 flatpak install flathub org.videolan.VLC
 flatpak install flathub com.calibre_ebook.calibre
 flatpak install flathub com.bitwarden.desktop
@@ -79,6 +80,6 @@ flatpak install flathub com.stremio.Stremio
 sudo apt update -qq && sudo apt upgrade -y && sudo apt autoremove -y
 
 # Run Python script:
-wget https://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@raw.githubusercontent.com/minsungson/ubuntu22-Setup-XPS13/master/main.py && sudo chmod +x main.py && ./main.py
+curl -O https://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@raw.githubusercontent.com/minsungson/ubuntu22-Setup-XPS13/master/main.py && sudo chmod +x main.py && ./main.py
 
-echo $'\n'$"Ready for REBOOT, remember to setup Howdy after reboot by running sudo howdy add, or adjust settings in config"
+sudo reboot now
