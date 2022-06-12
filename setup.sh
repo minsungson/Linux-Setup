633eb76d910647458fb82bdd8c259090b77a3ff7
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-06-12 19:21:55 +0100

    added dependancy

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -ex
 
 read -p "Enter your username: " target_user;
 
@@ -16,10 +15,23 @@ sudo add-apt-repository universe -y
 sudo add-apt-repository multiverse -y
 sudo add-apt-repository restricted -y
 
+# Install apps apt:
+
+sudo apt install ubuntu-restricted-extras -y
+sudo apt install preload -y
+sudo apt install gnome-tweaks -y
+sudo apt install gnome-shell-extensions -y
+sudo apt install bleachbit -y
+sudo apt install protonvpn -y
+sudo apt install libreoffice -y
+sudo apt install libfprint-2-tod1 -y
+sudo apt install git -y
+sudo apt install curl -y
+sudo apt install wget -y
+
 # Add fingerprint driver
 wget http://dell.archive.canonical.com/updates/pool/public/libf/libfprint-2-tod1-goodix/libfprint-2-tod1-goodix_0.0.4-0ubuntu1somerville1_amd64.deb
 sudo dpkg -i libfprint-2-tod1-goodix_0.0.4-0ubuntu1somerville1_amd64.deb
-
 
 sudo apt update && sudo apt upgrade -y
 
@@ -27,17 +39,13 @@ if isinstalled snap;
 then
 
 # Remove default Snap packages
-sudo snap remove chromium
-sudo snap remove emote
-sudo snap remove spotify
-sudo snap remove vlc
-sudo snap remove firefox
 sudo snap remove firefox
 sudo snap remove snap-store
 sudo snap remove gtk-common-themes
 sudo snap remove gnome-3-38-2004
-sudo snap remove core18
+sudo snap remove core20
 sudo snap remove snapd-desktop-integration
+sudo snap remove snapd
 
 # Remove Snap
 
@@ -64,38 +72,15 @@ fi
 sudo add-apt-repository ppa:mozillateam/ppa
 sudo apt install firefox
 
-# Remove packages:
-
-sudo apt remove rhythmbox -y -q
-
 # Add Flatpak support:
 
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Install apps apt:
-
-sudo apt install ubuntu-restricted-extras -y
-sudo apt install preload -y
-sudo apt install gnome-tweaks -y
-sudo apt install gnome-shell-extensions -y
-sudo apt install bleachbit -y
-sudo apt install protonvpn -y
-sudo apt install libreoffice -y
-sudo apt install git -y
-sudo apt install curl -y
-sudo apt install wget -y
-
 # Setup Git
 git config --global user.name "Minsung Son"
 git config --global user.email 100154286+minsungson@users.noreply.github.com
-
-# Install Openssh-Server
-
-sudo apt install openssh-server -y
-sudo systemctl enable ssh
-sudo systemctl start ssh
 
 # Install apps with flatpak:
 
@@ -112,6 +97,27 @@ flatpak install flathub com.stremio.Stremio -y
 "gsettings set org.gnome.desktop.background picture-uri file:////home/${target_user}/Pictures/penguin.jpeg";
 "unzip Gruvbox-Dark.zip -d /home/${target_user}/.themes/";
 "gsettings set org.gnome.desktop.interface gtk-theme 'Gruvbox-Dark'";
+
+sudo sh -c 'cat > .themes/CustomShellTheme/gnome-shell/gnome-shell.css << EOF
+
+ #panel {
+  background-color: rgba(42, 40, 39, 0.9);
+  font-weight: bold;
+  height: 30px;
+  color: rgba(221, 199, 161, 1);
+  font-feature-settings: 'tnum';
+  transition-duration: 250ms;
+  font-size: 10.5pt;
+  margin: 1px 1px;
+  margin-top: 0px;
+  margin-right: 0px;
+  margin-left: 0px;
+  border: 1px solid rgba(221, 199, 161, 0.5);
+  border-radius: 20px;
+}
+
+EOF'
+fi
 
 # Prepare to reboot:
 sudo apt update -qq && sudo apt upgrade -y && sudo apt autoremove -y
