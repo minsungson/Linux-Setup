48b2be6819f679a6f2fde400d48a4c18ae5713c8
Minsung Son <100154286+minsungson@users.noreply.github.com>
2022-06-29 00:47:18 +0100

    remove bloat and enable gnome theming

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ else
     exit 1;
 fi
 echo $"Machine will reboot after this script has been excecuted."
+read -n 1 -s -r -p "Press any key to continue"
+
 
 # Ensure repositories are enabled
 sudo add-apt-repository universe -y
@@ -35,8 +37,6 @@ sudo dpkg -i libfprint-2-tod1-goodix_0.0.4-0ubuntu1somerville1_amd64.deb
 
 sudo apt update && sudo apt upgrade -y
 
-if isinstalled snap;
-then
 
 # Remove default Snap packages
 
@@ -91,37 +91,62 @@ flatpak install flathub com.bitwarden.desktop -y
 flatpak install flathub com.stremio.Stremio -y
 flatpak install flathub net.ankiweb.Anki -y
 
+# Remove bloat
+
+sudo apt remove --purge rhythmbox -y
+sudo apt remove --purge gnome-sudoku -y
+sudo apt remove --purge gnome-mahjongg -y  
+sudo apt remove --purge gnome-mines -y
+sudo apt remove --purge remmina -y
+sudo apt remove --purge gnome-font-viewer -y
+sudo apt remove --purge gnome-characters -y
+sudo apt remove --purge gnome-calculator -y
+
+
 # Theming
-"cp penguin.jpeg ~/Pictures";
-"gsettings set org.gnome.desktop.background picture-uri file:////home/${target_user}/Pictures/penguin.jpeg";
-"unzip Gruvbox-Dark.zip -d /home/${target_user}/.themes/";
-"gsettings set org.gnome.desktop.interface gtk-theme 'Gruvbox-Dark'";
+gsettings set org.gnome.desktop.background picture-uri-dark file:///home/{target_user}/Downloads/penguin.jpeg"gsettings set org.gnome.desktop.background picture-uri file:////home/${target_user}/Pictures/penguin.jpeg"
+sudo unzip '/home/Linux-Setup' -d '/home/${target_user}/.themes'
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position LEFT
+gsettings set org.gnome.shell.extensions.dash-to-dock animation-time 0.1
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+gsettings set org.gnome.shell.extensions.dash-to-dock hide-delay 0.1
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-dominant-color true
+gsettings set org.gnome.shell.extensions.dash-to-dock show-show-apps-button false
+gsettings set org.gnome.shell.extensions.dash-to-dock activate-single-window false
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 30
+gsettings set org.gnome.desktop.sound input-feedback-sounds false
+gsettings set org.gnome.desktop.privacy report-technical-problems false
+gsettings set org.gnome.desktop.privacy remove-old-temp-files true
+gsettings set org.gnome.desktop.peripherals.touchpad speed 0.19852941176470584
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.interface icon-theme 'Yaru-prussiangreen'
+gsettings set org.gnome.desktop.interface gtk-theme 'Yaru-prussiangreen-dark'
+gsettings set org.gnome.desktop.interface gtk-color-palette 'black:white:gray50:red:purple:blue:light blue:green:yellow:orange:lavender:brown:goldenrod4:dodger blue:pink:light green:gray10:gray30:gray75:gray90'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface clock-show-seconds true
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.datetime automatic-timezone true
+gsettings set org.gnome.software show-ratings true
+gsettings set org.gnome.shell.extensions.ding show-home false
+gsettings set org.gnome.shell.extensions.ding start-corner 'top-right'
+gsettings set org.gnome.desktop.interface font-name 'Google Sans Text 10'
+gsettings set org.gnome.shell.extensions.user-theme name 'Gruvbox-Dark-B-Gnome-Shell'
 
-sudo sh -c 'cat > .themes/CustomShellTheme/gnome-shell/gnome-shell.css << EOF
-
- #panel {
-  background-color: rgba(42, 40, 39, 0.9);
-  font-weight: bold;
-  height: 30px;
-  color: rgba(221, 199, 161, 1);
-  font-feature-settings: 'tnum';
-  transition-duration: 250ms;
-  font-size: 10.5pt;
-  margin: 1px 1px;
-  margin-top: 0px;
-  margin-right: 0px;
-  margin-left: 0px;
-  border: 1px solid rgba(221, 199, 161, 0.5);
-  border-radius: 20px;
-}
-
-EOF'
-fi
+# Move fonts
+mkdir /usr/share/fonts
+cp -r /home/${target_user}/Linux-Setup/'Google Sans Text Regular.ttf' /usr/share/fonts
+cp -r /home/${target_user}/Linux-Setup/'Google Sans Text Medium.ttf' /usr/share/fonts
+cp -r /home/${target_user}/Linux-Setup/'Google Sans Text Medium Italic.ttf' /usr/share/fonts
+cp -r /home/${target_user}/Linux-Setup/'Google Sans Text Italic.ttf' /usr/share/fonts
+cp -r /home/${target_user}/Linux-Setup/'Google Sans Text Bold.ttf' /usr/share/fonts
+cp -r /home/${target_user}/Linux-Setup/'Google Sans Text Bold Italic.ttf' /usr/share/fonts
 
 # Prepare to reboot:
 sudo apt update -qq && sudo apt upgrade -y && sudo apt autoremove -y
 
-# Run Python script - OBSOLETE:
-# curl -O https://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@raw.githubusercontent.com/minsungson/ubuntu22-Setup-XPS13/master/main.py && sudo chmod +x main.py && ./main.py
+# Run Python script
+# curl -O https://raw.githubusercontent.com/minsungson/ubuntu22-Setup-XPS13/master/main.py && sudo chmod +x main.py && ./main.py
 
 sudo reboot now
